9140c5702314389da965e659b8d4bd3455544fca
mattstah77 <mattstah77@gmail.com>
2026-04-01 05:26:23 +0300

    commit

--- client/src/hooks/useTelegram.ts
+++ client/src/hooks/useTelegram.ts
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useMemo, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import type { TelegramTheme } from '../types/booking';
 
 // Светлая тема
